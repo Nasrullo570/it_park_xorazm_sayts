b20b394ac664184e033155d4ce89c1380527b646
Azamatmasharipov707 <azeraligarx@gmail.com>
2023-01-23 12:05:51 +0500

    Azamat katta

--- apps.js
+++ apps.js
@@ -98,3 +98,44 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+
+
+
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+const mainContent = document.querySelector('.main-content')
+
+console.log(mainContent)
+
+// Add an event listener listening for scroll
+mainContent.addEventListener("scroll", navHighlighter);
+
+
+function navHighlighter() {
+  // console.log('asd')
+  
+  // Get current scroll position
+  let scrollY = mainContent.scrollTop;
+  console.log(scrollY)
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 1600;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
